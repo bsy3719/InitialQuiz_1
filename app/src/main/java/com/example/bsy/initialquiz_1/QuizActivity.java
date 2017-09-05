d6a98fd8a57a0598a558d027d02ac2d3fe92037a
bsy3719 <bsy3719@naver.com>
2017-09-05 15:17:20 +0900

    리워드 광고 삭제 / 전면광고, 타이머 프로그레스바 추가

--- app/src/main/java/com/example/bsy/initialquiz_1/QuizActivity.java
+++ app/src/main/java/com/example/bsy/initialquiz_1/QuizActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -12,7 +14,9 @@ import android.widget.Toast;
 
 import com.example.bsy.initialquiz_1.Database.QuizBaseHelper;
 import com.example.bsy.initialquiz_1.databinding.ActivityQuizBinding;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
@@ -21,7 +25,7 @@ import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class QuizActivity extends AppCompatActivity implements RewardedVideoAdListener{
+public class QuizActivity extends AppCompatActivity {
 
     private ActivityQuizBinding binding;
 
@@ -30,26 +34,43 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
 
     private MethodUtils methodUtils = MethodUtils.getInstance();
 
-    private RewardedVideoAd mAd;
+    private InterstitialAd mInterstitialAd;
+
+    int value = 60; // progressBar 값
+    int add = 1; // 증가량, 방향
+
+    Handler handler;
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_quiz);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_quiz);
 
-        MobileAds.initialize(this, "ca-app-pub-4711336970766023~9674070775");
+        //전면광고
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        AdRequest request = new AdRequest.Builder().
+                addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(request);
 
-        //광고 설정 리스너 부분
-        mAd = MobileAds.getRewardedVideoAdInstance(this);
-        mAd.setRewardedVideoAdListener(this);
-        loadAd();
+        //전면광고 reload하는 부분
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
 
+        });
+
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        //최초 실행인지 체크
         InitialCheck();
-
-        //초성 추출하여 힌트 셋팅
-        binding.initialTextView.setText(methodUtils.getInitial(quizs.get(mCurrentIndex).getAnswer()));
-        //binding.initialTextView.setText("테스트");
+        //프로그레스바 실행
+        runProgressBar();
 
         binding.confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,13 +82,13 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
                 //정답을 맞췄을 경우
                 if (answer.equals(quizs.get(mCurrentIndex).getAnswer())) {
 
-                    toast = Toast.makeText(QuizActivity.this, quizs.get(mCurrentIndex).getId()+"번문제 정답!!!", Toast.LENGTH_SHORT);
+                    toast = Toast.makeText(QuizActivity.this, quizs.get(mCurrentIndex).getId() + "번문제 정답!!!", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER, 0, 150);
                     toast.show();
 
                     updateQuiz();
 
-                }else{
+                } else {
 
                     toast = Toast.makeText(QuizActivity.this, "오답!!!", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER, 0, 150);
@@ -77,20 +98,67 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
             }
         });
 
-        binding.adButton.setOnClickListener(new View.OnClickListener() {
+        //동영상 광고 버튼
+        binding.rewardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("@@@", "버튼클릭");
-                Log.d("@@@", String.valueOf(mAd.isLoaded()));
-               if (mAd.isLoaded()){
-                   mAd.show();
-                   Log.d("@@@", "광고뜸");
-               }
+
             }
         });
 
+        //전면 광고 버튼
+        binding.interstitialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+        });
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Toast.makeText(QuizActivity.this, "0초", Toast.LENGTH_SHORT).show();
+            }
+        };
+
     }
     //onCreate 끝
+
+    private void runProgressBar(){
+
+        handler = new Handler();
+        binding.timerProgressBar.setProgress(value);
+
+        // 앱시작시, Thread 를 시작해서 ProgressBar 를 증가시키기
+        // Thread 내부에서 화면에 작업을 하려면 Handler 를 사용해야한다
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() { // Thread 로 작업할 내용을 구현
+                while(true) {
+                    try {
+
+                        Thread.sleep(1000); // 시간지연
+
+                        value = value - add;
+
+                        binding.timerProgressBar.setProgress(value);
+
+                    } catch (InterruptedException e) { }
+
+                    if (value == 0){
+                        handler.sendMessage(handler.obtainMessage());
+                        break;
+                    }
+
+                } // end of while
+            }
+        });
+
+        thread.start(); // 쓰레드 시작
+
+    }
 
     private void updateQuiz() {
 
@@ -103,7 +171,7 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
 
     }
 
-    private void InitialCheck(){
+    private void InitialCheck() {
 
         //DB활성화
         QuizBaseHelper quizBaseHelper = new QuizBaseHelper(this);
@@ -111,19 +179,19 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
         //최초 실행 여부 판단하는 구문
         SharedPreferences pref = getSharedPreferences("isInitial", Activity.MODE_PRIVATE);
         boolean first = pref.getBoolean("isInitial", false);
-        if(first==false) {
+        if (first == false) {
             SharedPreferences.Editor editor = pref.edit();
             editor.putBoolean("isInitial", true);
             editor.commit();
 
-            quizBaseHelper.addQuiz(new Quiz("선풍기" , "여름", "회전"));
+            quizBaseHelper.addQuiz(new Quiz("선풍기", "여름", "회전"));
             quizBaseHelper.addQuiz(new Quiz("모니터", "컴퓨터", "화면"));
             quizBaseHelper.addQuiz(new Quiz("자스민", "커피", "부산대"));
             quizBaseHelper.addQuiz(new Quiz("휴대폰", "삼성", "애플"));
             quizBaseHelper.addQuiz(new Quiz("컴퓨터", "키보드", "마우스"));
 
             Log.d("@@@", "최초실행");
-        }else{
+        } else {
             Log.d("@@@", "최초실행이 아님");
         }
 
@@ -136,53 +204,4 @@ public class QuizActivity extends AppCompatActivity implements RewardedVideoAdLi
 
     }
 
-    private void loadAd(){
-        if(!mAd.isLoaded()){
-            mAd.loadAd("ca-app-pub-3940256099942544/5224354917", new AdRequest.Builder().build());
-        }
-    }
-
-    //리워드 보상 관련
-    @Override
-    public void onRewardedVideoAdLoaded() {
-        binding.adButton.setEnabled(true);
-    }
-
-    @Override
-    public void onRewardedVideoAdOpened() {
-
-    }
-
-    @Override
-    public void onRewardedVideoStarted() {
-
-    }
-
-    @Override
-    public void onRewardedVideoAdClosed() {
-        binding.adButton.setEnabled(false);
-        loadAd();
-    }
-
-    @Override
-    public void onRewarded(RewardItem rewardItem) {
-        Toast.makeText(QuizActivity.this, String.format(Locale.getDefault() , "You got %d %s!", rewardItem.getAmount(), rewardItem.getType()), Toast.LENGTH_SHORT).show();
-        //rewardItem.getAmount();
-
-    }
-
-    @Override
-    public void onRewardedVideoAdLeftApplication() {
-
-    }
-
-    @Override
-    public void onRewardedVideoAdFailedToLoad(int i) {
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
 }
